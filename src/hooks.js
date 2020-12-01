c63ab5fd68fc987a00076b3c279a04ded158de7e
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-12-01 14:46:57 -0600

    hooks.js 2

--- src/hooks.js
+++ src/hooks.js
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 
-export default function useHover() {
+const useHover = () => {
   const ref = useRef();
-  const {hovered, setHovered} = useState(false);
+  const [ hovered, setHovered ] = useState(false);
 
   function enter() {
     setHovered(true);
@@ -13,13 +13,15 @@ export default function useHover() {
 
   useEffect(() => {
     const refCopy = ref;
-    refCopy.current.addEventListener('mouseenter', enter)
-    refCopy.current.addEventListener('mouseleave', () => {
+    refCopy.current.addEventListener('mouseenter', enter);
+    refCopy.current.addEventListener('mouseleave', leave);
 
-    })
     return () => {
-
+      refCopy.current.removeEventListener('mouseenter', enter);
+      refCopy.current.removeEventListener('mouseleave', leave);
     }
   })
   return ( [ref, hovered] );
 };
+
+export default useHover;
